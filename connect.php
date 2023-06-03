d6a932a821b75a2e66d3bd51f23de7216083d8af
nazim <nazim.saifi1908@gmail.com>
2023-06-03 13:00:51 +0530

    first commit

---
+++ connect.php
@@ -0,0 +1,321 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="Upgrade yourself and get ready to explore the thriving opportunities with SkillOceans. We are a one-of-a-kind training institute that aims at refining your skills and making you future-ready.">
+    <meta name="keywords" content="Upgrade yourself and get ready to explore the thriving opportunities with SkillOceans. We are a one-of-a-kind training institute that aims at refining your skills and making you future-ready.">
+      <meta name="author" content="Explore our range of skill training courses now!">
+   <title>skilloceans</title>
+    <link rel="icon" type="image/x-icon" href="/asset1/LOGO.png">   <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+<link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+   <!------------------------>
+   <!-- nav start -->
+   <!------------------------->
+    <nav>
+      <div class="wrapper">
+        <div class="logo"><a href="./index.html"><img src="./asset1/LOGO.png" alt="" height="50px" width="100px"></a></div>
+        <input type="radio" name="slider" id="menu-btn" >
+        <input type="radio" name="slider" id="close-btn">
+        <ul class="nav-links">
+          <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+        <li><a href="./index.html">Home</a></li>
+           <li><a href="./aboutus.html">About</a></li>
+      
+          <!-- for WEB  -->
+          <li>
+            <a href="#" class="desktop-item">Web Development <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg></a>
+            <input type="checkbox" id="showweb">
+            <label for="showweb" class="mobile-ite">Web Courses</label>
+            <div class="web-box">
+              <div class="webcontent">
+                <div class="row">
+                  <img src="./asset1/nav web main.jpg" alt="">
+                </div>
+                <div class="row">
+                  <header>Frontend Development </header>
+                  <ul class="web-links">
+                    <li><a href="./hypertext,ark.html">HTML</a></li>
+                    <li><a href="css.html">CSS</a></li>
+                    <li><a href="javascript.html">Java Script</a></li>
+                    <li><a href="./bootstrap.html">Bootstrap</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>Backend Development</header>
+                  <ul class="web-links">
+                 <li><a href="./python.html">Python</a></li>
+                 <li><a href="./java.html">JAVA</a></li>
+                 <li><a href="./php.html">PHP</a></li>
+                 <li><a href="./sql.html">SQL</a></li>
+                 <li><a href="./nodejs.html">Node Js</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>Full Stack</header>
+                  <ul class="web-links">
+                    <li><a href="./mean.html">MEAN</a></li>
+                    <li><a href="./mern.html">MERN</a></li>
+             
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </li>
+          <!-- for Cad -->
+          <li>
+            <a href="#" class="desktop-item">CAD Courses <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg></a>
+            <input type="checkbox" id="showMega">
+            <label for="showMega" class="mobile-item">CAD</label>
+            <div class="mega-box">
+              <div class="content">
+                <div class="row">
+                  <img src="asset1/nav cad main.jpg" alt="">
+                </div>
+                <div class="row">
+                  <header>CIVIL CAD </header>
+                  <ul class="mega-links">
+                    <li><a href="./revit.html">Revit</a></li>
+                    <li><a href="./3dsmax.html">3ds Max</a></li>
+                    <li><a href="./sketchup.html">Sketch up</a></li>
+                    <li><a href="./maya.html">Maya</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>MECHANICAL CAD</header>
+                  <ul class="mega-links">
+                    <li><a href="./solidwork.html">Solid work</a></li>
+                    <li><a href="./nxcad.html">NX Cad</a></li>
+                    <li><a href="./catia.html">Catia</a></li>
+                    <li><a href="./creo.html">Creo</a></li>
+                    <!-- <li><a href="#">Press tool design (Die)</a></li> -->
+                    <li><a href="./molddesign.html">Mold Design</a></li>
+                    <li><a href="./nxcam.html">NX Cam</a></li>
+                  </ul>
+                </div>
+                <div class="row">
+                  <header>ELECTRICAL CAD</header>
+                  <ul class="mega-links">
+                    <li><a href="./electricalcad.html">AutoCAD Electrical </a></li>
+             
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </li>
+             <!-- For Animation -->
+             <li>
+              <a href="#" class="desktop-items">Animation Courses <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+              </svg></a>
+              <input type="checkbox" id="showbeba">
+              <label for="showbeba" class="mobile-items">Animation</label>
+              <div class="beba-box">
+                <div class="cont">
+                  <div class="row">
+                    <img src="asset1/nav animation main.jpg"  height="100px" width="100px" alt="">
+                  </div>
+                  <div class="row">
+                    <header>Graphic Designing </header>
+                    <ul class="beba-links">
+                      <li><a href="./photoshop.html">Adobe Photoshop</a></li>
+                      <li><a href="illustrator.html">Adobe Illustrator</a></li>
+                      <li><a href="Indesign.html">InDesign</a></li>
+                      <li><a href="./coraldraw.html">Coral Draw</a></li>
+                    </ul>
+                  </div>
+                  <div class="row">
+                    <header>Motion Graphic</header>
+                    <ul class="beba-links">
+                      <li><a href="./dreamweaver.html">Dreamweaver</a></li>
+                      <li><a href="./animatecc.html">Amimate CC</a></li>
+                      <li><a href="./audition.html">Audition</a></li>
+                      <li><a href="aftereffect.html">After Effect</a></li>
+                    
+                    </ul>
+                  </div>
+                  <div class="row">
+                    <header>3D</header>
+                    <ul class="beba-links">
+                      <li><a href="./3DSsmax.html">3ds Max</a></li>
+                      <li><a href="./mayas.html">Maya</a></li>
+                      <li><a href="./sketchups.html">Sketch Up</a></li>
+                      <li><a href="./lumion.html">Lumion</a></li>
+               
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </li>
+        
+          <li><a href="./blog.html">Blog</a></li>
+        </ul>
+        <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+      </div>
+    </nav>
+<!------------------------>
+ <!-- nav end -->
+ <!------------------------->
+   
+  <div class="connect-php">
+    <div class="container">
+<div class="row">
+    <div class="col-12">
+       
+  
+
+      <h1 style="font-weight: bold;" class="text-center m-5">Thankyou For Submittion.....</h1>
+
+    </div>
+   
+</div>
+    </div>
+  </div>
+   <!-- connect content start -->
+
+
+
+  
+    
+  <!-- ======= Footer ======= -->
+    <footer id="footer">
+<div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>skilloceans<span>.</span></h3>
+            <p>
+              3rd Floor, K S Corporate Tower, 24B, Film City, Sector 16A, Noida, Uttar Pradesh 201301<br>
+              <br>
+               
+          <a style="color: #fff;" href="https://wa.me/918447670174">   <strong>Phone: </strong>+91 8447670174 <br></a> 
+             <a style="color: #fff;" href=""> <strong>Email: </strong>info@skilloceans.com<br></a>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+                 <li><i class="bx bx-chevron-right"></i> <a href="./index.html">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="./aboutus.html">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Social Networks</h4>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+            <div class="social-links mt-3">
+              <a href="https://twitter.com/NazimSa10578774?t=0J37vpxDbCfZ5YXC8nX4zw&s=09" class="twitter"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+              </svg></a>
+              <a href="https://www.facebook.com/people/Skill-Oceans/100086475786201/" class="facebook"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+              </svg></a>
+              <a href="https://www.instagram.com/skilloceans__/" class="instagram"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+              </svg></a>
+              <a href="https://join.skype.com/invite/xo0DylUAWkJ7" class="google-plus"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-skype" viewBox="0 0 16 16">
+                <path d="M4.671 0c.88 0 1.733.247 2.468.702a7.423 7.423 0 0 1 6.02 2.118 7.372 7.372 0 0 1 2.167 5.215c0 .344-.024.687-.072 1.026a4.662 4.662 0 0 1 .6 2.281 4.645 4.645 0 0 1-1.37 3.294A4.673 4.673 0 0 1 11.18 16c-.84 0-1.658-.226-2.37-.644a7.423 7.423 0 0 1-6.114-2.107A7.374 7.374 0 0 1 .529 8.035c0-.363.026-.724.08-1.081a4.644 4.644 0 0 1 .76-5.59A4.68 4.68 0 0 1 4.67 0zm.447 7.01c.18.309.43.572.729.769a7.07 7.07 0 0 0 1.257.653c.492.205.873.38 1.145.523.229.112.437.264.615.448.135.142.21.331.21.528a.872.872 0 0 1-.335.723c-.291.196-.64.289-.99.264a2.618 2.618 0 0 1-1.048-.206 11.44 11.44 0 0 1-.532-.253 1.284 1.284 0 0 0-.587-.15.717.717 0 0 0-.501.176.63.63 0 0 0-.195.491.796.796 0 0 0 .148.482 1.2 1.2 0 0 0 .456.354 5.113 5.113 0 0 0 2.212.419 4.554 4.554 0 0 0 1.624-.265 2.296 2.296 0 0 0 1.08-.801c.267-.39.402-.855.386-1.327a2.09 2.09 0 0 0-.279-1.101 2.53 2.53 0 0 0-.772-.792A7.198 7.198 0 0 0 8.486 7.3a1.05 1.05 0 0 0-.145-.058 18.182 18.182 0 0 1-1.013-.447 1.827 1.827 0 0 1-.54-.387.727.727 0 0 1-.2-.508.805.805 0 0 1 .385-.723 1.76 1.76 0 0 1 .968-.247c.26-.003.52.03.772.096.274.079.542.177.802.293.105.049.22.075.336.076a.6.6 0 0 0 .453-.19.69.69 0 0 0 .18-.496.717.717 0 0 0-.17-.476 1.374 1.374 0 0 0-.556-.354 3.69 3.69 0 0 0-.708-.183 5.963 5.963 0 0 0-1.022-.078 4.53 4.53 0 0 0-1.536.258 2.71 2.71 0 0 0-1.174.784 1.91 1.91 0 0 0-.45 1.287c-.01.37.076.736.25 1.063z"/>
+              </svg></a>
+              <a href="#" class="linkedin"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+              </svg></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+<hr>
+    <div class="container py-4">
+      <div class="copyright">
+           &copy; Copyright <strong><span>skilloceans</span></strong>. All Rights Reserved 
+      </div>
+     
+    </div>
+  </footer><!-- End Footer --> 
+
+     <div  id="fixedfooter" class="fixed-footer">
+        <form style="display: flex; justify-content: space-evenly;" action="connect.php" method="post">
+
+        
+          <input class="hidefixedfooter" style="color: #fda206;" type="text"   id="firstName" name="firstName" placeholder="Name" required/>
+          <input class="hidefixedfooter" style="color: #fda206;" type="text" id="lastName"name="lastName" placeholder="Course" required/>
+    
+          <input class="hidefixedfooter" style="color: #fda206;"  type="text" id="email" name="email" placeholder="E-mail" required/>
+     
+     
+          <input class="hidefixedfooter" style="color: #fda206;" type="number" id="number" name="number" placeholder="Phone no." required/>
+        <input class="hidefixedfooter" style="background-color: #fda206;" type="submit" class="btn  px-4" />
+        <button class="hideopenform"  type="button" class="btn bg-light py-0" data-bs-toggle="modal" data-bs-target="#openstuform">Open Form</button>
+
+      </form>
+      </div>
+
+
+
+         <!-- bottom to top -->
+      <button onclick="topFunction()" id="myBtn" title="Go to top">
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+        </svg>
+      </button>
+      <script src="./coursetest/illustrator.js"></script>
+
+
+
+
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$email = $_POST['email'];
+	$number = $_POST['number'];
+//   print_r($number);die;
+	// Database connection
+	$conn = new mysqli('localhost','u707760839_students', '$cX^?iT4', 'u707760839_registration'); 
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+	     
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, email, number) values(?, ?, ?, ?)");
+		$stmt->bind_param("sssi", $firstName, $lastName, $email, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "We will contact you as soon as possible....";
+		$stmt->close();
+		$conn->close();
+	}
+?>
---
+++ script.js
@@ -0,0 +1,51 @@
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// fixed footer pop up form
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+// bottom to top button code
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+
